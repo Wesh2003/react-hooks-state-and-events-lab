35668b0a29b1c15203a62bbefc52b558b3ac34d9
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-11-14 12:31:28 +0300

    Completed Assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { useState } from 'react';
+
 
 function Item({ name, category }) {
+
+  const[bought, setBought] = useState([])
   return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
+    <div>
+      <li className="">
+        <span>{name}</span>
+        <span className="category">{category}</span>
+        <button className="add" onclick = {setBought(item.name)}>Add to Cart</button>
+      </li>
+
+      <li className="in-cart">
+      {bought}
+      </li>
+
+      <li className="">
+      {/* the item is NOT in the cart */}
+      </li>
+      </div>
+
   );
+
+
+
+
 }
 
 export default Item;
